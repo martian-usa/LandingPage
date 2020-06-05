0737f1ed92d77c1f77a4ff2b43d3595d04c4dcac
Mikita <nik.waterpolo@gmail.com>
2020-06-05 18:00:36 -0400

    added style and func

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-function test() {
-  console.log('test');
-}
+let ticking = false;
 
-//something
+// Cache elements
+const header = document.getElementsByTagName("header")[0]; // We're interested in the first element
+
+// Activation Margins
+const HEADER_MARGIN = 65;
+const SECTION_MARGIN = 300;
+
+const scrollActions = () => {
+  // 1. Sets whether the header is visible or not
+  if (window.scrollY > HEADER_MARGIN) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+  ticking = false;
+};
+
+window.addEventListener("scroll", () => {
+  if (!ticking) {
+    window.requestAnimationFrame(scrollActions);
+    ticking = true;
+  }
+});
+scrollActions();
